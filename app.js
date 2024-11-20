d13da6f8e36f2ab22a886fd01ee57afaadadf6fb
byrv <bhyravprasad256@gmail.com>
2024-11-20 22:29:42 +0530

    Add Cors Header

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://my-fever-app-front-end.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use('/api/temperature', temperatureRoutes);
 
 app.get('/', (req, res) => {
